8da78407a618c1ed440a4b0a44bdca1116b331d4
Bryan Kahlig <bryan.kahlig@gmail.com>
2014-12-28 15:09:35 -0600

    Added some denon goodies. Fixed some problems with compilation.

--- irTestHardCoded/irTestHardCoded.ino
+++ irTestHardCoded/irTestHardCoded.ino
@@ -8,9 +8,13 @@ void setup() {
 ////////////////////////////////////
 // DENON
 ////////////////////////////////////
-#define   KEY_POWER                0x000000000000221C        #  Was: power-on
-#define   KEY_POWER                0x000000000000211C        #  Was: power-off
+#define   KEY_POWER                0x000000000000221C        //  Was: power-on
+#define   KEY_POWER                0x000000000000211C        //  Was: power-off
+#define   KEY_MUTE                 0x000000000000203C        //  Was: mute
+
+IRsend irsend;
 
 void loop() {
-  
+  irsend.sendDenon(KEY_MUTE, 15);
+  delay(10000);
 }
