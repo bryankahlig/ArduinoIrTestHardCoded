f2b3b6bbf616b46a9a202aeee52797bdc7333444
Bryan Kahlig <bryan.kahlig@gmail.com>
2015-01-04 10:24:55 -0600

    Added more Denon codes.

--- irTestHardCoded/irTestHardCoded.ino
+++ irTestHardCoded/irTestHardCoded.ino
@@ -8,9 +8,29 @@ void setup() {
 ////////////////////////////////////
 // DENON sendDenon
 ////////////////////////////////////
-#define   DENON_KEY_POWER                0x000000000000221C        //  Was: power-on
-#define   DENON_KEY_POWER                0x000000000000211C        //  Was: power-off
-#define   DENON_KEY_MUTE                 0x000000000000203C        //  Was: mute
+#define DENON_POWER_ON               0x221C
+#define DENON_POWER_OFF              0x211C
+#define DENON_MUTE                   0x203C
+#define DENON_UP                     0x1B14
+#define DENON_DOWN                   0x1894
+#define DENON_LEFT                   0x1BF8
+#define DENON_RIGHT                  0x22EC
+#define DENON_SELECT                 0x201C
+#define DENON_VOLUME_UP              0x223C
+#define DENON_VOLUME_DOWN            0x213C
+#define DENON_SOURCE_SELECT_TUNER    0x228C
+#define DENON_SOURCE_SELECT_CD       0x208C
+#define DENON_SOURCE_SELECT_PHONO    0x230C
+#define DENON_SOURCE_SELECT_DVD      0x231C
+#define DENON_SOURCE_SELECT_VDP      0x214C
+#define DENON_SOURCE_SELECT_AUX      0x20CC
+#define DENON_SOURCE_SELECT_VCR1     0x22CC
+#define DENON_SOURCE_SELECT_VCR2     0x21CC
+#define DENON_SOURCE_SELECT_TAPE     0x212C
+#define DENON_SOURCE_SELECT_DBS      0x204C
+#define DENON_SOURCE_SELECT_TV       0x224C
+
+
 
 ///////////////////////////////////
 // VIZIO sendNEC/sendNEC(REPEAT,0)
@@ -61,15 +81,19 @@ void setup() {
 IRsend irsend;
 
 void loop() {
-//  irsend.sendDenon(KEY_MUTE, DENON_BITS);
+    irsend.sendDenon(DENON_KEY_MUTE, DENON_BITS);
+    delay(5000);
+
 //  irsend.sendNEC(VIZIO_MENU, 32);
 //  irsend.sendNEC(REPEAT,0);
 //  delay(3000);
 //  irsend.sendNEC(VIZIO_EXIT, 32);
 //  irsend.sendNEC(REPEAT,0);
 //  delay(5000);
-  irsend.sendNEC(ROKU_PAUSE, 32);
-  delay(5000);
-  irsend.sendNEC(ROKU_PAUSE, 32);
-  delay(10000);
+
+  // ROKU TESTING
+//  irsend.sendNEC(ROKU_PAUSE, 32);
+//  delay(5000);
+//  irsend.sendNEC(ROKU_PAUSE, 32);
+//  delay(10000);
 }
